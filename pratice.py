6da0ccd8413c52a4e5e15f8abf95a7384b3cd079
GUR-DEV-eng <singh84596@gmail.com>
2026-03-08 17:55:27 +0530

    Implement digit frequency count from input
    Add frequency counting of digits from input string
    
    Signed-off-by: GUR-DEV-eng <singh84596@gmail.com>

--- pratice.py
+++ pratice.py
@@ -1,2 +1,11 @@
-for i in range(1,11):
-    print(i)
+n = input().strip()      # read as string
+
+freq = [0] * 10          # freq[0]..freq[9]
+
+for ch in n:
+    if ch.isdigit():
+        d = int(ch)
+        freq[d] += 1
+
+# print counts of 0 to 9
+print(*freq)
